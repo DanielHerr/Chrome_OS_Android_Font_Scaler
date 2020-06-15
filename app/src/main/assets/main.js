d9b40cdd98b2f9a44db17c0e16bc9be5e8434bb4
Daniel Herr <danielherrsoftware@gmail.com>
2020-06-14 22:07:38 -0500

    Align slider labels, Set slider to current font size

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -4,8 +4,16 @@ if(android.canModifySettings() == false) {
  explanation.showModal()
 }
 
+var size = Math.round(android.getFontSize() * 100)
+sizeslider.value = size
+sizeoutput.value = size
+
 grant.addEventListener("click", function() {
  android.openSettingsPermission()
+})
+
+explanation.addEventListener("cancel", function(event) {
+ event.preventDefault()
 })
 
 sizeslider.addEventListener("input", function() {
